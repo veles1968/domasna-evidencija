0b1a9d2168ce9d185a7224ce983505dc444203ec
veles1968 <dimce.tumbov@gmail.com>
2021-02-21 19:06:03 +0100

    14. Verzija 21.02.2021 19:05

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     user: process.env.DE_USER,
     password: process.env.DE_PASSWORD,
     connectString: process.env.DE_CONNECTIONSTRING,
-    libraryDir: "C:\\PRIVAT_DATA\\TOOLS\\ORACLE_instantclient_19_9",
+    // libraryDir: "C:\\PRIVAT_DATA\\TOOLS\\ORACLE_instantclient_19_9",
     poolMin: 10,
     poolMax: 10,
     poolIncrement: 0,
