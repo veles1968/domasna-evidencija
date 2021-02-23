66f57e42a051667fceeba13dc33cdd8e9bc131c9
veles1968 <dimce.tumbov@gmail.com>
2021-02-23 18:26:51 +0100

    23.02.2021 18:26

--- config/database.js
+++ config/database.js
@@ -4,7 +4,9 @@ module.exports = {
     password: process.env.DE_PASSWORD,
     connectString: process.env.DE_CONNECTIONSTRING,
     // libraryDir: "C:\\PRIVAT_DATA\\TOOLS\\ORACLE_instantclient_19_9",
-    libraryDir: "$CLIENT_BUILD_DIR/instantclient",
+    // libraryDir: "$CLIENT_BUILD_DIR/instantclient",
+    libraryDir:
+      "$HOME/.oracle-build/instantclient/:/app/.apt/lib/x86_64-linux-gnu/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}",
     poolMin: 10,
     poolMax: 10,
     poolIncrement: 0,
