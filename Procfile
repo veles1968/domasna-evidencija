e777d8e2aa4964769d47bd2538702e3cdeb91be4
veles1968 <dimce.tumbov@gmail.com>
2021-02-24 19:39:55 +0100

    24.02.2021 02

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start
+web: ./client/expo start
--- client/app/navigation/routes.js
+++
@@ -1,6 +0,0 @@
-export default Object.freeze({
-  ARTIKAL_DETAILS: "ArtikalDetails",
-  ARTIKAL_EDIT: "ArtikalEdit",
-  PRIMANJA_EDIT: "PrimanjaEdit",
-  PRIMANJA_LIST: "Primanjas",
-});
